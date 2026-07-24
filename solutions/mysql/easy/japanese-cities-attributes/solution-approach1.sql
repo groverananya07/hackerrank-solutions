f1956c4c7eb093f09e7f6778529c966956d75b81
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-07-24 23:28:16 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-24, 11:28 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * FROM CITY 
+WHERE COUNTRYCODE='JPN';
