ce2fe2956aa2f6bd0459cd4055ef441f20030a88
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-07-24 23:30:40 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-24, 11:30 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT name FROM CITY
+WHERE countrycode='JPN';
