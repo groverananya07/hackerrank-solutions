a9f3f434a65c8ba154775e60b7e310d82b4d5755
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-07-25 01:26:03 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-25, 01:26 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT name from employee
+order by name
