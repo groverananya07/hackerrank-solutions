ce8f8c9f35290c257b2049e65dde11c14fc4d0b2
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-07-24 23:19:59 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-24, 11:19 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * FROM city
+WHERE ID=1661;
