92a1fe0152be69f1f901c74973cfb33856608baa
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-08-02 12:52:55 +0530

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-02, 12:52 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT ROUND(AVG(Population))
+FROM CITY
