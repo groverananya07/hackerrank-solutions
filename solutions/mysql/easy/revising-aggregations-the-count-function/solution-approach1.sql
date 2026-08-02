eae92f3b39c09b4ae83bec6bc8152a97a9f6f216
groverananya07 <164987524+groverananya07@users.noreply.github.com>
2026-08-02 11:00:22 +0530

    ✅ Revising Aggregations - The Count Function [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-the-count-function/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+-- Problem     Revising Aggregations - The Count Function
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-02, 11:00 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT COUNT(*)
+FROM CITY
+WHERE population>100000;
